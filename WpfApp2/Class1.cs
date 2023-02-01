6c61502aab7a42b89bb8259277818e64b79b21cc
upsfffaaa <114106587+upsfffaaa@users.noreply.github.com>
2023-02-01 15:17:34 +0300

    Add files via upload

---
+++ WpfApp2/Class1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+	public class TicTacToeGame
+	{
+		private static List<TicTacToeGame> OldGames = new List<TicTacToeGame>();
+		private int[,] gameTable;
+		private int turn;
+		private int steps;
+		private Line winLine;
+		public int[,] GameTable
+		{
+			get { return gameTable; }
+			private set { gameTable = value; }
+		}
+		public int Turn
+		{
+			get { return turn; }
+			private set { turn = value; }
+		}
+		public int Steps
+		{
+			get { return steps; }
+			private set { steps = value; }
+		}
+		public Line WinLine
+		{
+			get { return winLine; }
+			private set { winLine = value; }
+		}
+		Random rand;
+		public TicTacToeGame()
+		{
+			Steps = 0;
+			GameTable = new int[3, 3];
+			for (int i = 0; i < GameTable.GetLength(0); i++)
+				for (int j = 0; j < GameTable.GetLength(1); j++)
+					GameTable[i, j] = 0;
+			rand = new Random();
+			if (rand.Next(2) == 1)
+				Turn = 1;
+			else Turn = -1;
+		}
+		private void ChangeTurn()
+		{
+			Turn *= -1;
+		}
+		public bool Play(int player, int i, int j)
+		{
+			GameTable[i, j] = player; 
+			steps++;
+			Line Winner = CheckWinner();
+
+			if (Winner != null)
+			{
+				WinLine = Winner;
+				throw new Exception("we have winner");
+			}
+
+			ChangeTurn(); 
+			return true;
+		}
+		public TicTacToeGame GameOver()
+		{
+			OldGames.Add(this);
+			return new TicTacToeGame();
+		}
+		public void DoTurnRobot()
+		{
+			int[] OptionToWin = freeCells(Turn);
+			if (OptionToWin != null)
+			{ Play(Turn, OptionToWin[0], OptionToWin[1]); return; }
+			int[] OptionToBlock = freeCells(Turn * -1);
+			if (OptionToBlock != null)
+			{ Play(Turn, OptionToBlock[0], OptionToBlock[1]); return; }
+			if (steps == 0) 
+			{
+				RandomOX();
+
+				return;
+			}
+			if (steps == 1) 
+			{
+				if (gameTable[1, 1] != 0)
+				{
+					RandomOX();
+					return;
+				}
+				if (gameTable[0, 0] != 0 || gameTable[2, 2] != 0 || gameTable[2, 0] != 0 || gameTable[0, 2] != 0)
+				{
+					Play(turn, 1, 1);
+					return;
+				}
+			}
+			Play(turn, rand.Next(2), rand.Next(2));
+		}
+		private bool RandomOX()
+		{
+			int a = 0, b = 0;
+			if (rand.Next(2) == 1)
+				a = 2;
+			if (rand.Next(2) == 1)
+				b = 2;
+			return Play(Turn, a, b);
+		}
+		private int[] freeCells(int xo)
+		{
+			for (int i = 0; i < GameTable.GetLength(0); i++)
+			{
+				if (GameTable[i, 0] == xo && GameTable[i, 1] == xo && GameTable[i, 2] == 0)
+					return new int[] { i, 2 };
+				else if (GameTable[i, 1] == xo && GameTable[i, 2] == xo && GameTable[i, 0] == 0)
+					return new int[] { i, 0 };
+				else if (GameTable[i, 0] == xo && GameTable[i, 2] == xo && GameTable[i, 1] == 0)
+					return new int[] { i, 1 };
+			}
+n:			for (int i = 0; i < GameTable.GetLength(1); i++)
+			{
+				if (GameTable[0, i] == xo && GameTable[1, i] == xo && GameTable[2, i] == 0)
+					return new int[] { 2, i };
+				else if (GameTable[1, i] == xo && GameTable[2, i] == xo && GameTable[0, i] == 0)
+					return new int[] { 0, i };
+				else if (GameTable[0, i] == xo && GameTable[2, i] == xo && GameTable[1, i] == 0)
+					return new int[] { 1, i };
+			}
+			if (GameTable[0, 0] == xo && GameTable[2, 2] == xo && GameTable[1, 1] == 0)
+				return new int[] { 1, 1 };
+			else if (GameTable[1, 1] == xo && GameTable[2, 2] == xo && GameTable[0, 0] == 0)
+				return new int[] { 0, 0 };
+			else if (GameTable[0, 0] == xo && GameTable[1, 1] == xo && GameTable[2, 2] == 0)
+				return new int[] { 2, 2 };
+
+			if (GameTable[2, 0] == xo && GameTable[0, 2] == xo && GameTable[1, 1] == 0)
+				return new int[] { 1, 1 };
+			else if (GameTable[1, 1] == xo && GameTable[0, 2] == xo && GameTable[2, 0] == 0)
+				return new int[] { 2, 0 };
+			else if (GameTable[2, 0] == xo && GameTable[1, 1] == xo && GameTable[0, 2] == 0)
+				return new int[] { 0, 2 };
+			return null;
+		}
+		private Line CheckWinner()
+		{
+			for (int i = 0; i < GameTable.GetLength(0); i++)
+			{
+				if (GameTable[i, 0] == Turn && GameTable[i, 1] == Turn && GameTable[i, 2] == Turn)
+					return new Line()
+					{
+						X1 = 0,
+						Y1 = 40 + 80 * i,
+						X2 = 240,
+						Y2 = 40 + 80 * i
+					};
+			}
+			for (int i = 0; i < GameTable.GetLength(1); i++)
+			{
+				if (GameTable[0, i] == Turn && GameTable[1, i] == Turn && GameTable[2, i] == Turn)
+					return new Line()
+					{
+						X1 = 40 + 80 * i,
+						Y1 = 0,
+						X2 = 40 + 80 * i,
+						Y2 = 240
+					};
+			}
+			if (GameTable[0, 0] == Turn && GameTable[2, 2] == Turn && GameTable[1, 1] == Turn)
+				return new Line()
+				{
+					X1 = 0,
+					Y1 = 0,
+					X2 = 240,
+					Y2 = 240
+				};
+			if (GameTable[2, 0] == Turn && GameTable[0, 2] == Turn && GameTable[1, 1] == Turn)
+				return new Line()
+				{
+					X1 = 0,
+					Y1 = 240,
+					X2 = 240,
+					Y2 = 0
+				};
+			return null;
+		}
+	}
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace TicTacToe
+{
+	public partial class MainWindow : Window
+	{
+		TicTacToeGame BoardData;
+		Button[,] BoardView;
+		public MainWindow()
+		{
+			InitializeComponent();
+			BoardData = new TicTacToeGame();
+			CreateGameTable();
+			UpdateTable();
+
+			if (BoardData.Turn == 1)
+				RobotPlay();
+		}
+		private void CreateGameTable()
+		{
+			BoardView = new Button[3, 3];
+			BoardView[0, 0] = Place00;
+			BoardView[0, 1] = Place01;
+			BoardView[0, 2] = Place02;
+			BoardView[1, 0] = Place10;
+			BoardView[1, 1] = Place11;
+			BoardView[1, 2] = Place12;
+			BoardView[2, 0] = Place20;
+			BoardView[2, 1] = Place21;
+			BoardView[2, 2] = Place22;
+		}
+		private void UpdateTable()
+		{
+			if (BoardData.Turn == 1)
+				WhoIsNow.Text = "first move - O";
+			else WhoIsNow.Text ="first move - X";
+			for (int i = 0; i < BoardView.GetLength(0); i++)
+				for (int j = 0; j < BoardView.GetLength(1); j++)
+				{
+					if (BoardData.GameTable[i, j] == 1)
+					{
+						BoardView[i, j].Content = "O";
+						BoardView[i, j].IsEnabled = false;
+					}
+					else if (BoardData.GameTable[i, j] == -1)
+					{
+						BoardView[i, j].Content = "X";
+						BoardView[i, j].IsEnabled = false;
+					}
+					else
+					{
+						BoardView[i, j].Content = "";
+						BoardView[i, j].IsEnabled = true;
+					}
+				}
+		}
+		private void One_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Button Selected = sender as Button;
+				int i = -1, j = -1;
+				bool found = false;
+				for (i = 0; i < BoardView.GetLength(0) && !found; i++)
+					for (j = 0; j < BoardView.GetLength(1) && !found; j++)
+						if (Selected.Name == BoardView[i, j].Name) found = true;
+				i--; j--;
+				BoardData.Play(BoardData.Turn, i, j);
+				UpdateTable();
+
+				RobotPlay();
+			}
+			catch (Exception ex)
+			{
+				if (ex.Message == "we have winner")
+					SomeoneWon();
+				StatusAndErrors.Text = ex.Message;
+			}
+		}
+		private void RobotPlay()
+		{
+			try
+			{
+				BoardData.DoTurnRobot();
+				UpdateTable();
+			}
+			catch (Exception ex)
+			{
+				StatusAndErrors.Text = ex.Message;
+				if (ex.Message == "we have winner")
+				{ SomeoneWon(); return; }
+				if (BoardData.Steps < 9)
+					RobotPlay();
+			}
+		}
+		private void SomeoneWon()
+		{
+			UpdateTable();
+			WinnerLineView.X1 = BoardData.WinLine.X1;
+			WinnerLineView.X2 = BoardData.WinLine.X2;
+			WinnerLineView.Y1 = BoardData.WinLine.Y1;
+			WinnerLineView.Y2 = BoardData.WinLine.Y2;
+			WinnerLineView.Visibility = Visibility.Visible;
+			for (int i = 0; i < BoardView.GetLength(0); i++)
+				for (int j = 0; j < BoardView.GetLength(1); j++)
+					BoardView[i, j].IsEnabled = false;
+		}
+		private void GameOverButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.BoardData = BoardData.GameOver();
+			WinnerLineView.Visibility = Visibility.Collapsed;
+			UpdateTable();
+		}
+	}
+}
+
